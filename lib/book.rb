fc1249838bb2e5fcdfa1dd03eb02a8827a1916a0
kiran ram <kram@lions.molloy.edu>
2020-02-29 17:36:00 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,38 @@
-
+class Book
+  def initialize(title)
+    @title = title
+    
+    def author=(author)
+      @author = author
+    end
+    
+    def author 
+      @author
+    end
+    
+    def page_count=(num)
+      @page_count = num
+    end
+    
+    def page_count
+      @page_count
+    end
+    
+    def genre=(genre)
+      @genre = genre
+    end
+    
+    def genre 
+      @genre
+    end
+    
+    def turn_page
+       puts "Flipping the page...wow, you read fast!"
+    end
+    
+    def title 
+      @title 
+    end
+ end
+end
 
